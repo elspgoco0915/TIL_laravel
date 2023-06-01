02e6b9226f3bd3dd99ea3c08dc12d8becc2fc768
elspgoco0915 <elspgoco0915@gmail.com>
2023-06-01 13:05:07 +0900

    #1 環境構築用にlaravel初期状態にした

--- _web/backend/routes/web.php
+++ _web/backend/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +16,3 @@ use App\Http\Controllers\PostController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('posts', PostController::class);
